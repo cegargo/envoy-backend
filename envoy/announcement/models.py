4ee9273f6ce049e34f0394c9c006548c2bce7350
cegargo <clintegargo@protonmail.ch>
2023-01-11 12:05:47 +0800

    Announcements

--- envoy/announcement/models.py
+++ envoy/announcement/models.py
@@ -1,11 +1,22 @@
 from django.db import models
+from user.models import Faculty
 
 
 class Announcement(models.Model):
-    title = models.CharField(max_length=128)
-    college = models.CharField(max_length=64)
-    url = models.URLField(max_length=200)
-    posted_date = models.DateField()
+    AnnouncementTitle = models.CharField(max_length=128)
+    PostDateTime = models.DateTimeField(auto_now_add=True)
+    AnnouncementType = models.CharField(max_length=64)
+    Author = models.ForeignKey(
+        Faculty, on_delete=models.CASCADE)
+    NumberOfViewers = models.IntegerField(default=0)
+    Duration = models.TimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return self.AnnouncementType
+
+
+class AnnouncementContent(models.Model):
+    AnnouncementID = models.ForeignKey(Announcement, on_delete=models.CASCADE)
+    Description = models.CharField(max_length=2048)
+    Image = models.ImageField(max_length=2048)
+    URL = models.URLField(max_length=2048)
---
+++ envoy/announcement/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from announcement.models import Announcement, AnnouncementContent
+
+
+class AnnouncementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Announcement
+        fields = ('AnnouncementTitle', 'Author', 'AnnouncementType')
+
+
+class AnnouncementContentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AnnouncementContent
+        fields = ('Description', 'Image', 'URL')
