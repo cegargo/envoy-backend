53717d0333aeab33a09d4e491a5a808539ad233f
cegargo <clintegargo@protonmail.ch>
2023-01-11 12:06:25 +0800

    User

--- envoy/user/models.py
+++ envoy/user/models.py
@@ -1,6 +1,4 @@
-import uuid
 from django.db import models
-from django.contrib.auth.models import User
 
 
 class User(models.Model):
@@ -12,3 +10,26 @@ class User(models.Model):
 
     def __str__(self):
         return self.email
+
+
+class Student(models.Model):
+    StudentName = models.CharField(max_length=128)
+    CourseEnrolled = models.CharField(max_length=128)
+    StudentEmail = models.CharField(max_length=128)
+    StudentPassword = models.CharField(max_length=32)
+    DateRegistered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.StudentEmail
+
+
+class Faculty(models.Model):
+    FacultyName = models.CharField(max_length=128)
+    CourseAdministered = models.CharField(max_length=64)
+    FacultyEmail = models.CharField(max_length=128)
+    FacultyPassword = models.CharField(max_length=32)
+    NumberOfPosts = models.IntegerField(default=0)
+    DateRegistered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.FacultyEmail
--- envoy/user/serializers.py
+++ envoy/user/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
-from user.models import User
+from user.models import User, Faculty, Student
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('full_name', 'college', 'email')
+
+
+class FacultySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Faculty
+        fields = ('FacultyName', 'CourseAdministered',
+                  'FacultyEmail', 'FacultyPassword')
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ('StudentName', 'CourseEnrolled',
+                  'StudentEmail', 'StudentPassword')
